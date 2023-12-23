6b359dd01f79163be82d714b4e9d25cea92fafdd
isology <isoalper-068@hotmail.com>
2023-12-23 14:42:17 +0300

    Update chainingHash.java

--- src/chainingHash.java
+++ src/chainingHash.java
@@ -4,37 +4,71 @@ class WordElement{
 	String word;
 	int length;
 
-	public WordElement(String word) {
-		this.word = word;
-		this.length = word.length();
-	}
+    public WordElement(String word) {
+        this.word = word;
+        this.length = word.length();
+      }
 }
 
 class ChainingMapOfWords{
 	int size = 0;
-	ArrayList<WordElement> WordMap = new ArrayList<>();
+	 ArrayList<WordElement> WordMap = new ArrayList<>();
 }
 
 public class chainingHash{
 	ChainingMapOfWords[] table = new ChainingMapOfWords[100];
 	Graph graph;
-
+		
 	public void initializeTable() {
 		for(int i=0; i<100; i++) {
 			table[i] = new ChainingMapOfWords();
 		}
 	}
-
+	
 	public char lowerChar(char letter) {
-		return Character.toLowerCase(letter);
-	}
-
+    	return Character.toLowerCase(letter);
+    }
+	
 	public int findIndexOfLetter(char letter) {
 		return letter - 'a';
 	}
-
+	
 	public void insertWord(String word) {
 		WordElement wordElement = new WordElement(word);
 		table[wordElement.length].WordMap.add(wordElement);
+    }
+	 	
+	
+	
+	public int letterToInt(char src) {
+		switch(src) {
+		case 'q': return 0;
+		case 'w': return 1;
+		case 'e': return 2;
+		case 'r': return 3;
+		case 't': return 4;
+		case 'y': return 5;
+		case 'u': return 6;
+		case 'i': return 7;
+		case 'o': return 8;
+		case 'p': return 9;
+		case 'a': return 10;
+		case 's': return 11;
+		case 'd': return 12;
+		case 'f': return 13;
+		case 'g': return 14;
+		case 'h': return 15;
+		case 'j': return 16;
+		case 'k': return 17;
+		case 'l': return 18;
+		case 'z': return 19;
+		case 'x': return 20;
+		case 'c': return 21;
+		case 'v': return 22;
+		case 'b': return 23;
+		case 'n': return 24;
+		case 'm': return 25;
+		default: return -1;
+		}
 	}
 }
