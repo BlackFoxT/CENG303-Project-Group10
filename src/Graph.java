03d6a75bd34c1a82c4dcfccd02bcfb6329ad11cd
Hidayet <125586147+BlackFoxT@users.noreply.github.com>
2023-12-23 14:10:21 +0300

    Update Graph.java
    Defining Graph Class

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class Graph {
-   
+    boolean isEnded = false;
+    ArrayList<Integer> path;
+	ArrayList<Node> nodes;
+	int[][] matrix;
+	int steps = 0;
+	int counts = 0;
+	
+	Graph(int size){
+		nodes = new ArrayList<>();
+		matrix = new int[size][size];
+	}
+	
 }
