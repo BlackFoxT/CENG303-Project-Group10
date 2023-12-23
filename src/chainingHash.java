972b7a2cf7e1e6d6f251a7201b85e66c4eebc0a0
isology <isoalper-068@hotmail.com>
2023-12-23 14:32:17 +0300

    Update chainingHash.java
    adding chaining hash

--- src/chainingHash.java
+++ src/chainingHash.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
 
 class WordElement{
+	String word;
+	int length;
 
+    public WordElement(String word) {
+        this.word = word;
+        this.length = word.length();
+      }
 }
 
 class ChainingMapOfWords{
-	
+	int size = 0;
+	 ArrayList<WordElement> WordMap = new ArrayList<>();
 }
 
 public class chainingHash{
+	ChainingMapOfWords[] table = new ChainingMapOfWords[100];
+	Graph graph;
 	
 }
