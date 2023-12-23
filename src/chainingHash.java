fa55bdddeaa5e89cb9d2fb8ecb3a57310b2a1f86
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-12-23 23:48:05 +0300

    Adding createKeyboardGraph function to create a graph for English keyboard letters
    This function is for creating a keyboard graph that has 26 vertices for English letters and the edges between each vertex to its neighbors are added as well. We will use this graph to calculate the distance between letters.

--- src/chainingHash.java
+++ src/chainingHash.java
@@ -25,6 +25,193 @@ public class chainingHash{
 		}
 	}
 	
+	 public void createKeyboardGraph() {
+		int vertices = 26;
+		Graph graph1 = new Graph(vertices);
+	      
+	         graph1.addNode(new Node(1));
+	         graph1.addNode(new Node(2));
+	         graph1.addNode(new Node(3));
+	         graph1.addNode(new Node(4));
+	         graph1.addNode(new Node(5));
+	         graph1.addNode(new Node(6));
+	         graph1.addNode(new Node(7));
+	         graph1.addNode(new Node(8));
+	         graph1.addNode(new Node(9));
+	         graph1.addNode(new Node(10));
+	         graph1.addNode(new Node(11));
+	         graph1.addNode(new Node(12));
+	         graph1.addNode(new Node(13));
+	         graph1.addNode(new Node(14));
+	         graph1.addNode(new Node(15));
+	         graph1.addNode(new Node(16));
+	         graph1.addNode(new Node(17));
+	         graph1.addNode(new Node(18));
+	         graph1.addNode(new Node(19));
+	         graph1.addNode(new Node(20));
+	         graph1.addNode(new Node(21));
+	         graph1.addNode(new Node(22));
+	         graph1.addNode(new Node(23));
+	         graph1.addNode(new Node(24));
+	         graph1.addNode(new Node(25));
+	         graph1.addNode(new Node(26));
+	         
+	         graph1.addEdge(0, 1);
+	         graph1.addEdge(0, 10);
+	         graph1.addEdge(0, 11);
+	         
+	         graph1.addEdge(1, 0);
+	         graph1.addEdge(1, 2);
+	         graph1.addEdge(1, 10);
+	         graph1.addEdge(1, 11);
+	         graph1.addEdge(1, 12);
+	         
+	         graph1.addEdge(2, 1);
+	         graph1.addEdge(2, 3);
+	         graph1.addEdge(2, 11);
+	         graph1.addEdge(2, 12);
+	         graph1.addEdge(2, 13);
+	         
+	         graph1.addEdge(3, 2);
+	         graph1.addEdge(3, 4);
+	         graph1.addEdge(3, 12);
+	         graph1.addEdge(3, 13);
+	         graph1.addEdge(3, 14);
+	         
+	         graph1.addEdge(4, 3);
+	         graph1.addEdge(4, 5);
+	         graph1.addEdge(4, 13);
+	         graph1.addEdge(4, 14);
+	         graph1.addEdge(4, 15);
+	         
+	         graph1.addEdge(5, 4);
+	         graph1.addEdge(5, 6);
+	         graph1.addEdge(5, 14);
+	         graph1.addEdge(5, 15);
+	         graph1.addEdge(5, 16);
+	         
+	         graph1.addEdge(6, 5);
+	         graph1.addEdge(6, 7);
+	         graph1.addEdge(6, 15);
+	         graph1.addEdge(6, 16);
+	         graph1.addEdge(6, 17);
+	         
+	         graph1.addEdge(7, 6);
+	         graph1.addEdge(7, 8);
+	         graph1.addEdge(7, 16);
+	         graph1.addEdge(7, 17);
+	         graph1.addEdge(7, 18);
+	         
+	         graph1.addEdge(8, 9);
+	         graph1.addEdge(8, 7);
+	         graph1.addEdge(8, 17);
+	         graph1.addEdge(8, 18);
+	         
+	         graph1.addEdge(9, 8);
+	         graph1.addEdge(9, 17);
+	         graph1.addEdge(9, 18);
+	         
+	         graph1.addEdge(10, 0);
+	         graph1.addEdge(10, 1);
+	         graph1.addEdge(10, 11);
+	         graph1.addEdge(10, 19);
+	         graph1.addEdge(10, 20);
+	         
+	         graph1.addEdge(11, 10);
+	         graph1.addEdge(11, 12);
+	         graph1.addEdge(11, 1);
+	         graph1.addEdge(11, 0);
+	         graph1.addEdge(11, 2);
+	         graph1.addEdge(11, 19);
+	         graph1.addEdge(11, 20);
+	         
+	         graph1.addEdge(12, 1);
+	         graph1.addEdge(12, 2);
+	         graph1.addEdge(12, 3);
+	         graph1.addEdge(12, 11);
+	         graph1.addEdge(12, 13);
+	         graph1.addEdge(12, 20);
+	         
+	         graph1.addEdge(13, 2);
+	         graph1.addEdge(13, 4);
+	         graph1.addEdge(13, 12);
+	         graph1.addEdge(13, 14);
+	         graph1.addEdge(13, 21);
+	         graph1.addEdge(13, 22);
+	         
+	         graph1.addEdge(14, 3);
+	         graph1.addEdge(14, 4);
+	         graph1.addEdge(14, 5);
+	         graph1.addEdge(14, 13);
+	         graph1.addEdge(14, 15);
+	         graph1.addEdge(14, 22);
+	         
+	         graph1.addEdge(15, 4);
+	         graph1.addEdge(15, 6);
+	         graph1.addEdge(15, 6);
+	         graph1.addEdge(15, 14);
+	         graph1.addEdge(15, 16);
+	         graph1.addEdge(15, 23);
+	         graph1.addEdge(15, 24);
+	         
+	         graph1.addEdge(16, 5);
+	         graph1.addEdge(16, 6);
+	         graph1.addEdge(16, 7);
+	         graph1.addEdge(16, 15);
+	         graph1.addEdge(16, 17);
+	         graph1.addEdge(16, 24);
+	         graph1.addEdge(16, 25);
+	         
+	         graph1.addEdge(17, 16);
+	         graph1.addEdge(17, 18);
+	         graph1.addEdge(17, 7);
+	         graph1.addEdge(17, 6);
+	         graph1.addEdge(17, 8);
+	         graph1.addEdge(17,25);
+	         
+	         graph1.addEdge(18, 7);
+	         graph1.addEdge(18, 8);
+	         graph1.addEdge(18, 9);
+	         graph1.addEdge(18, 17);
+	         
+	         graph1.addEdge(19, 10);
+	         graph1.addEdge(19, 11);
+	         graph1.addEdge(19, 20);
+	         
+	         graph1.addEdge(20, 19);
+	         graph1.addEdge(20, 21);
+	         graph1.addEdge(20, 11);
+	         graph1.addEdge(20, 12);
+	         
+	         graph1.addEdge(21, 20);
+	         graph1.addEdge(21, 22);
+	         graph1.addEdge(21, 12);
+	         graph1.addEdge(21, 13);
+	         
+	         graph1.addEdge(22, 21);
+	         graph1.addEdge(22, 23);
+	         graph1.addEdge(22, 13);
+	         graph1.addEdge(22, 14);
+	         
+	         graph1.addEdge(23, 22);
+	         graph1.addEdge(23, 24);
+	         graph1.addEdge(23, 14);
+	         graph1.addEdge(23, 15);
+	         
+	         graph1.addEdge(24, 23);
+	         graph1.addEdge(24, 25);
+	         graph1.addEdge(24, 15);
+	         graph1.addEdge(24, 16);
+	         
+	         graph1.addEdge(25, 24);
+	         graph1.addEdge(25, 16);
+	         graph1.addEdge(25, 17);
+	         
+	         this.graph = graph1;
+		}
+	
+	
+	
 	public char lowerChar(char letter) {
     	return Character.toLowerCase(letter);
     }
@@ -59,6 +246,7 @@ public class chainingHash{
 		return false;
     }
 
+	
 	public int letterToInt(char src) {
 		switch(src) {
 		case 'q': return 0;
@@ -87,7 +275,7 @@ public class chainingHash{
 		case 'b': return 23;
 		case 'n': return 24;
 		case 'm': return 25;
-		default: return -1;
+		default: return -1;
 		}
 	}
 }
