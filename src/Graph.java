9a47df66bf0e0d69c58d858528eeb97c94a81383
Zubair1683 <134060453+Zubair1683@users.noreply.github.com>
2023-12-23 14:19:17 +0300

    Adding some important function of graph
    Adding addNode, addEdge, checkEdge and getPath for adding node and edge to graph and checking the edge of nodes, and getting the path that I will use it to get the shortest path and show it in the map.

--- src/Graph.java
+++ src/Graph.java
@@ -5,12 +5,30 @@ public class Graph {
     ArrayList<Integer> path;
 	ArrayList<Node> nodes;
 	int[][] matrix;
-	int steps = 0;
-	int counts = 0;
 	
 	Graph(int size){
 		nodes = new ArrayList<>();
 		matrix = new int[size][size];
 	}
 	
+	public void addNode(Node node) {
+		nodes.add(node);
+	}
+	
+	public void addEdge(int src, int dst) {
+		matrix[src][dst] = 1;
+	}
+	
+	public boolean checkEdge(int src, int dst) {
+		if(matrix[src][dst] == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public ArrayList<Integer> getPath(){
+		return path;
+	}
 }
