ff23f0992b079029bbbc2cfabb0f99ce115a5c0b
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2023-12-23 14:37:37 +0300

    .

--- src/chainingHash.java
+++ src/chainingHash.java
@@ -4,19 +4,37 @@ class WordElement{
 	String word;
 	int length;
 
-    public WordElement(String word) {
-        this.word = word;
-        this.length = word.length();
-      }
+	public WordElement(String word) {
+		this.word = word;
+		this.length = word.length();
+	}
 }
 
 class ChainingMapOfWords{
 	int size = 0;
-	 ArrayList<WordElement> WordMap = new ArrayList<>();
+	ArrayList<WordElement> WordMap = new ArrayList<>();
 }
 
 public class chainingHash{
 	ChainingMapOfWords[] table = new ChainingMapOfWords[100];
 	Graph graph;
-	
+
+	public void initializeTable() {
+		for(int i=0; i<100; i++) {
+			table[i] = new ChainingMapOfWords();
+		}
+	}
+
+	public char lowerChar(char letter) {
+		return Character.toLowerCase(letter);
+	}
+
+	public int findIndexOfLetter(char letter) {
+		return letter - 'a';
+	}
+
+	public void insertWord(String word) {
+		WordElement wordElement = new WordElement(word);
+		table[wordElement.length].WordMap.add(wordElement);
+	}
 }
