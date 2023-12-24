e311c8484f91cff6b2cec6241765a2997b1fa2e5
isology <isoalper-068@hotmail.com>
2023-12-24 18:16:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		chainingHash dictionary = new chainingHash();
+        
+		dictionary.initializeTable();
+			String line;
+			BufferedReader myReader = new BufferedReader(new FileReader("words.txt"));
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			while ((line = myReader.readLine()) != null) {
+				dictionary.insertWord(line);
+			}
+			System.out.println("Enter a word or a sentence ");
+		Scanner input = new Scanner(System.in);
+	    String writenSentence = input.nextLine();
+	    
+	    dictionary.printCorrectedOne(writenSentence); 
 	}
 
 }
